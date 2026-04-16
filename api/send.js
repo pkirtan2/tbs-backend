85c0df52a104ff456f35f0f9a4d661c20273d1e7
unknown <pkirtan539@gmail.com>
2026-04-16 13:45:53 -0700

    fix smtp tls

--- api/send.js
+++ api/send.js
@@ -13,15 +13,17 @@ export default async function handler(req, res) {
     }
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.hostinger.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,   // 🔥 IMPORTANT FIX
+  },
+});
     await transporter.sendMail({
       from: `"Website Contact" <${process.env.EMAIL_USER}>`,
       to: process.env.EMAIL_USER,
